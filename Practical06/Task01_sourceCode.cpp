251135c986398b91f8107c9983ba9f3738bc7c17
Neeti <85558187+Neeti960@users.noreply.github.com>
2021-10-06 10:56:19 +0530

    Create Task01_sourceCode.cpp

---
+++ Practical06/Task01_sourceCode.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a= 6,b=2;
+float c=5.1,d=2.6;
+string s1="Neeti";
+string s2="Khushi";
+cout<<a+b<<endl;
+cout<<c+d<<endl;
+cout<<c+b<<endl;
+cout<<s1+s2<<endl;
+//cout<<s1+a<<endl;
+return 0;
+}
