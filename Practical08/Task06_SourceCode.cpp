5ce09218740b47656b77ecaca3339fd170f73de0
Neeti <85558187+Neeti960@users.noreply.github.com>
2022-01-09 21:52:48 +0530

    Create Task06_SourceCode.cpp

---
+++ Practical08/Task06_SourceCode.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void series(int n)
+{
+	if(n==1){
+		cout<<2<<" ";
+		return;
+	}
+	
+	
+	series(n-1);
+	cout<<(pow(n,n) +n)<<" ";
+	
+}		
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<"Series\n";
+	series(n);
+	return 0;
+}
+ 
