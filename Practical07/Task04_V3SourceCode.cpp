4734eb1d007b7e6f1d1953b1e0d5eabc20e193be
Neeti <85558187+Neeti960@users.noreply.github.com>
2022-01-09 20:19:49 +0530

    Create Task04_V3SourceCode.cpp

---
+++ Practical07/Task04_V3SourceCode.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+	using namespace std;
+	
+
+	int main()
+	{
+		int N = 3;
+		int arr[N][N];
+		for(int i = 0 ; i < N ; i++)
+	  {
+			for(int j = 0 ; j < N ; j++)
+				cin >> arr[i][j];
+		}
+	  for(int i = 0 ; i <= N ; i++)
+	  {
+			for(int j = 0 ; j <= N ; j++)
+				arr[i][j] = ((j*10+i)%10)+1;
+		}
+	  cout << "Resultant array : " << endl;
+		for(int i = 0 ; i < N ; i++)
+	  {
+			for(int j = 0 ; j < N ; j++)
+				cout << arr[i][j] << " ";
+			cout << "\n";
+		}
+	  return 0;
+	}
