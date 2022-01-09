6921d7e39455c464bfc7cf54b26d6d0d838c3ce2
Neeti <85558187+Neeti960@users.noreply.github.com>
2022-01-09 21:12:34 +0530

    Create Task08_SourceCode.cpp

---
+++ Practical07/Task08_SourceCode.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i,j,n;
+  cout<<"enter number of lines";
+  cin>>n;
+  for(i =1;i<=n;i++)
+  {
+      for(j=1;j<=n;j++)
+      {
+          if(j>=(n+2)-i)
+        cout<<" ";
+        else
+        cout<<"*";
+      }
+     cout<<endl;
+   }
+   return 0;
+}
+ 
