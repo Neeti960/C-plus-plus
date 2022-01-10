2c59e5f119aff918d3c589a21733157e761444b0
Neeti <85558187+Neeti960@users.noreply.github.com>
2022-01-10 08:01:34 +0530

    Create Task02_SourceCode.cpp

---
+++ Practical10/Task02_SourceCode.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	public:
+	string name;
+	int cls;
+	char sec;
+	int rollno;
+	void getdata(string s,int c,char sec,int r)
+	{
+		name=s;
+		cls=c;
+		this->sec=sec;
+		rollno=r;
+	}
+};
+int main()
+{
+	student s1;
+	s1.getdata("khushi",10,'C',31);
+	student *p;
+	p=&s1;
+	cout<<p->name<<endl;
+	cout<<p->cls<<endl;
+	cout<<p->sec<<endl;
+	cout<<p->rollno<<endl;
+}
+ 
