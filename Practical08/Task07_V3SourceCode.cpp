043391a9a1996d75de6cdffaa3df2accd7847ec7
Neeti <85558187+Neeti960@users.noreply.github.com>
2022-01-10 07:28:16 +0530

    Create Task07_V3SourceCode.cpp

---
+++ Practical08/Task07_V3SourceCode.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *a,int *b)
+{
+	int t=*a;
+	*a=*b;
+	*b=t;
+	
+}
+
+int main()
+{
+	int a,b;
+	cout<< "Enter the values of a and b \n";
+	cin>>a>>b;
+	cout<<"Before Swapping\na & b: "<<a<<" "<<b<<endl;
+	swap(&a,&b);
+	cout<<"After Swapping using pass by address\na & b: "<<a<<" "<<b<<endl;
+	
+}
+ 
