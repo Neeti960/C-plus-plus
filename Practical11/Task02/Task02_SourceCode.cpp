e1835dfb610fcee8294a01f771ecd2bf925c866b
Neeti <85558187+Neeti960@users.noreply.github.com>
2022-01-10 09:50:51 +0530

    Create Task02_SourceCode.cpp

---
+++ Practical11/Task02/Task02_SourceCode.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class MarineAnimals
+{
+public:
+void displayMA()
+{
+cout<<"I am Marine Animal"<<endl;
+}
+};
+class Mammals
+{
+public:
+void displayM()
+{
+cout<< "I am Mammal"<<endl;
+}
+};
+class BlueWhale : public MarineAnimals, public Mammals
+{
+public:
+void displayB()
+{
+cout<<"I belong to both categories Mammals as well as Marine Animals"<<endl;
+}
+};
+int main()
+{
+  Mammals ob1;
+  MarineAnimals ob2;
+  BlueWhale ob3;
+  ob1.displayM();
+  ob2.displayMA();
+  ob3.displayB();
+  ob3.displayM();
+  ob3.displayMA();
+  }
